414ef1ed4fa86104a195b42eab73d2ca0e94499b
marcmjn <marcmjn@yahoo.com>
2024-05-06 03:31:19 -0700

    sprint 14 mod1 done

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,30 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  // select * from accounts
+  return db('accounts')
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  //select * from accounts where id = 1
+  return db('accounts').where('id', id).first()
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async account => {
+  // insert into accounts (name, budget) values 
+  const [id] = await db('accounts').insert(account)
+  return getById(id)
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async (id, account) => {
+  // 
+  await db('accounts').where('id', id).update(account)
+  return getById(id)
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+  // delete from accounts where id = 1
+  return db('accounts').where('id', id).del()
 }
 
 module.exports = {
